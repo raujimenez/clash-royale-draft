a2a4b78c485cb7a10656fd543e69faf865294cfa
Raul Jimenez <officialrauljimenez@gmail.com>
2021-04-02 21:22:42 -0500

    added samedevice and differentdevice routes

--- src/App.js
+++ src/App.js
@@ -3,20 +3,31 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Announcer from "./components/Announcer";
-import LandingPageButtons from "./components/LandingPageButtons";
-
-// const accentColor = "#33b04a";
+import LandingPageNavigation from "./components/LandingPageNavigation";
+import PlayerDraft from "./components/PlayerDraft";
+import PlayerDraftChooser from "./components/PlayerDraftChooser";
 
 function App() {
   return (
+    <div>
+      <Announcer />
       <Router>
         <Switch>
           <Route path="/" exact>
-            <Announcer />
-            <LandingPageButtons />
+            <LandingPageNavigation />
+          </Route>
+          <Route path="/playerdraft/chooser" exact>
+            <PlayerDraftChooser />
+          </Route>
+          <Route path="/playerdraft/samedevice" exact>
+            <PlayerDraft />
+          </Route>
+          <Route path="/playerdraft/differentDevice" exact>
+            <div></div>
           </Route>
         </Switch>
       </Router>
+    </div>
   );
 }
 
