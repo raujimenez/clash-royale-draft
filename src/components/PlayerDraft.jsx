1e372eb46917cfed194609024b2b38d5c697713d
Raul Jimenez <officialrauljimenez@gmail.com>
2021-04-02 22:07:34 -0500

    added leftoverwork to be saved for later

--- src/components/PlayerDraft.jsx
+++ src/components/PlayerDraft.jsx
@@ -7,18 +7,21 @@ import PlayerXDraft from './PlayerXDraft';
 
 export default function PlayerDraftInput(props) {
     const [player1Id, setPlayer1Id] = useState('');
-    const [player2Id, setPlayer2id] = useState('');
+    const [player2Id, setPlayer2Id] = useState('');
+
+    const [player1Deck, setPlayer1Deck] = useState(null);
+    const [player2Deck, setPlayer2Deck] = useState(null);
 
     return (
         <Router>
             <Switch>
                 <Route path="/playerdraft/samedevice" exact>
                     <PlayerIdInput setPlayerId={setPlayer1Id} />
-                    <PlayerIdInput setPlayerId={setPlayer2id} />
-                    <LinkButton title="Start Draft" toLocation="/playerdraft/samedevice/start"/>
+                    <PlayerIdInput setPlayerId={setPlayer2Id} />
+                    <LinkButton title="Start Draft" toLocation="/playerdraft/samedevice/start" />
                 </Route>
                 <Route path="/playerdraft/samedevice/start" exact>
-                    <PlayerXDraft player1Id={player1Id} player2Id={player2Id}/>
+                    <PlayerXDraft />
                 </Route>
             </Switch>
         </Router>
